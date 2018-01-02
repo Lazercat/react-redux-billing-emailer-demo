bf7d66b964fde140fc7d872367c42a9be3e9759e
Jesse Lewis <jesseaustinlewis@gmail.com>
2018-01-02 10:53:03 -0600

    minor update to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (res, req) => {
-  res.send({ hi: 'there' });
+  res.send('peaches');
 });
 
 const PORT = process.env.PORT || 5000;
