5a5f0f46e847e0046ed9425d0aaf6511f105f110
Jesse Lewis <jesseaustinlewis@gmail.com>
2018-01-08 16:18:56 -0600

    update to route on api

--- index.js
+++ index.js
@@ -27,6 +27,19 @@ require('./routes/authRoutes')(app);
 //   res.send('howdy. I am app.');
 // });
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //set port and run server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
