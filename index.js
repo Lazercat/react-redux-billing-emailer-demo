3b870f747fa60df6d5e51959651b21d468a11749
Jesse Lewis <jesseaustinlewis@gmail.com>
2018-01-23 20:22:25 -0600

    update to express routes

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
 
   //express will serve up index.html file when it doesnt know the route.
   app.get('*', (req, res) => {
-    res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
